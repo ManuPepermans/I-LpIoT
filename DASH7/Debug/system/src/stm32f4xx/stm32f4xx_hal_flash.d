36090f509b79d7923afd44c0ce00d4e8930cb538
Manu Pepermans <manupepermans@Manus-MBP.fritz.box>
2017-11-18 13:42:42 +0100

    UART configuration for future use with DASH7 and LoraWan. Added a blinking led for GPIO testing.

---
+++ DASH7/Debug/system/src/stm32f4xx/stm32f4xx_hal_flash.d
@@ -0,0 +1,79 @@
+system/src/stm32f4xx/stm32f4xx_hal_flash.o: \
+ ../system/src/stm32f4xx/stm32f4xx_hal_flash.c \
+ ../system/include/stm32f4xx/stm32f4xx_hal.h \
+ ../include/stm32f4xx_hal_conf.h ../include/main.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_rcc.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_def.h \
+ ../system/include/cmsis/device/stm32f4xx.h \
+ ../system/include/cmsis/device/stm32f401xe.h \
+ ../system/include/cmsis/core_cm4.h \
+ ../system/include/cmsis/core_cmInstr.h \
+ ../system/include/cmsis/cmsis_gcc.h \
+ ../system/include/cmsis/core_cmFunc.h \
+ ../system/include/cmsis/core_cmSimd.h \
+ ../system/include/cmsis/device/system_stm32f4xx.h \
+ ../system/include/stm32f4xx/Legacy/stm32_hal_legacy.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_rcc_ex.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_gpio.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_gpio_ex.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_dma.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_dma_ex.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_cortex.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_flash.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_flash_ex.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_flash_ramfunc.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_pwr.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_pwr_ex.h \
+ ../system/include/stm32f4xx/stm32f4xx_hal_uart.h
+
+../system/include/stm32f4xx/stm32f4xx_hal.h:
+
+../include/stm32f4xx_hal_conf.h:
+
+../include/main.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_rcc.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_def.h:
+
+../system/include/cmsis/device/stm32f4xx.h:
+
+../system/include/cmsis/device/stm32f401xe.h:
+
+../system/include/cmsis/core_cm4.h:
+
+../system/include/cmsis/core_cmInstr.h:
+
+../system/include/cmsis/cmsis_gcc.h:
+
+../system/include/cmsis/core_cmFunc.h:
+
+../system/include/cmsis/core_cmSimd.h:
+
+../system/include/cmsis/device/system_stm32f4xx.h:
+
+../system/include/stm32f4xx/Legacy/stm32_hal_legacy.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_rcc_ex.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_gpio.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_gpio_ex.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_dma.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_dma_ex.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_cortex.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_flash.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_flash_ex.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_flash_ramfunc.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_pwr.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_pwr_ex.h:
+
+../system/include/stm32f4xx/stm32f4xx_hal_uart.h:
