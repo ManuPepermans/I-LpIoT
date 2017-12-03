f4221e9917ea425bd9bff3670c99ebe617f0f451
Lander Dellafaille <lander@dellafaille.me>
2017-12-03 10:46:42 +0100

    Parsing from incomming messages

--- Backend/backend.py
+++ Backend/backend.py
@@ -29,140 +29,168 @@ from tb_api_client import swagger_client
 from tb_api_client.swagger_client import ApiClient, Configuration
 from tb_api_client.swagger_client.rest import ApiException
 
+
 class Backend:
+    def __init__(self):
+        # use cmd settings in script
+        argparser = argparse.ArgumentParser()
+        argparser.add_argument("-v", "--verbose", help="verbose", default=False, action="store_true")
+        argparser.add_argument("-b", "--broker", help="mqtt broker hostname", default="localhost")
+        argparser.add_argument("-u", "--url", help="URL of the thingsboard server", default="http://localhost:8080")
+        argparser.add_argument("-t", "--token", help="token to access the thingsboard API", required=True)
+        argparser.add_argument("-U", "--user", help="username for MQTT broker", required=True)
+        argparser.add_argument("-P", "--password", help="password for MQTT broker", required=True)
+        argparser.add_argument("-n", "--node", help="node name", default="4337313400210032")
 
-  global teller
-  teller = 0
+        self.mq = None
+        self.connected_to_mqtt = False
 
-  def __init__(self):
-    #use cmd settings in script
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("-v", "--verbose", help="verbose", default=False, action="store_true")
-    argparser.add_argument("-b", "--broker", help="mqtt broker hostname", default="localhost")
-    argparser.add_argument("-u", "--url", help="URL of the thingsboard server", default="http://localhost:8080")
-    argparser.add_argument("-t", "--token", help="token to access the thingsboard API", required=True)
-    argparser.add_argument("-us", "--user", help="username for MQTT broker", required=True)
-    argparser.add_argument("-pw", "--password", help="password for MQTT broker", required=True)
-    argparser.add_argument("-n", "--node", help="node name", default="4337313400210032")
+        self.config = argparser.parse_args()
+        self.config.D7 = "/tb/#"
+        self.config.LOR = "/loriot/#"
+        self.connect_to_mqtt()
 
-    self.mq = None
-    self.connected_to_mqtt = False
+        api_client_config = Configuration()
+        api_client_config.host = self.config.url
+        api_client_config.api_key['X-Authorization'] = self.config.token
+        api_client_config.api_key_prefix['X-Authorization'] = 'Bearer'
+        api_client = ApiClient(api_client_config)
 
-    self.config = argparser.parse_args()
-    self.config.D7 = "/tb/#"
-    self.config.LOR = "/loriot/#"
-    self.connect_to_mqtt()
+        self.device_controller_api = swagger_client.DeviceControllerApi(api_client=api_client)
+        self.device_api_controller_api = swagger_client.DeviceApiControllerApi(api_client=api_client)
 
-    api_client_config = Configuration()
-    api_client_config.host = self.config.url
-    api_client_config.api_key['X-Authorization'] = self.config.token
-    api_client_config.api_key_prefix['X-Authorization'] = 'Bearer'
-    api_client = ApiClient(api_client_config)
+    def connect_to_mqtt(self):
+        self.connected_to_mqtt = False
 
-    self.device_controller_api = swagger_client.DeviceControllerApi(api_client=api_client)
-    self.device_api_controller_api =swagger_client.DeviceApiControllerApi(api_client=api_client)
+        # client_id=””, clean_session=True, userdata=None, protocol=MQTTv311, transport=”tcp”)
+        self.mq = mqtt.Client("", True, None, mqtt.MQTTv31)
+        self.mq.on_connect = self.on_mqtt_connect
+        self.mq.on_message = self.on_mqtt_message
+        self.mq.connect(self.config.broker, 1883, 60)
+        self.mq.loop_start()
+        while not self.connected_to_mqtt: pass  # busy wait until connected
+        print("Connected to MQTT broker on {}".format(
+            self.config.broker,
+        ))
 
-  def connect_to_mqtt(self):
-    self.connected_to_mqtt = False
+    def on_mqtt_connect(self, client, config, flags, rc):
+        print("Connect to topic: {}".format(self.config.D7))
+        # subscribe to topic
+        self.mq.subscribe(self.config.D7)
+        self.mq.subscribe(self.config.LOR)
+        # login with username and password
+        self.mq.username_pw_set(self.config.user, self.config.password)
+        self.connected_to_mqtt = True
 
-    # client_id=””, clean_session=True, userdata=None, protocol=MQTTv311, transport=”tcp”)
-    self.mq = mqtt.Client("", True, None, mqtt.MQTTv31)
-    self.mq.on_connect = self.on_mqtt_connect
-    self.mq.on_message = self.on_mqtt_message
-    self.mq.connect(self.config.broker, 1883, 60)
-    self.mq.loop_start()
-    while not self.connected_to_mqtt: pass  # busy wait until connected
-    print("Connected to MQTT broker on {}".format(
-      self.config.broker,
-    ))
+    def on_mqtt_message(self, client, config, msg):
 
-  def on_mqtt_connect(self, client, config, flags, rc):
-    print("Connect to topic: {}".format(self.config.D7))
-    # subscribe to topic
-    self.mq.subscribe(self.config.D7)
-    self.mq.subscribe(self.config.LOR)
-    # login with username and password
-    self.mq.username_pw_set(self.config.user, self.config.password)
-    self.connected_to_mqtt = True
+        # msg contains already parsed command in ALP in JSON
+        # print("ALP Command received from {}".format(msg.topic))
+        try:
+            obj = jsonpickle.json.loads(msg.payload)
+        except:
+            # print("Payload not valid JSON, skipping")
+            return
 
-  def on_mqtt_message(self, client, config, msg):
-    global teller
+        gateway = obj["deviceId"]
+        cmd = jsonpickle.decode(jsonpickle.json.dumps(obj["alp"]))
+        node_id = gateway  # overwritten below with remote node ID when received over D7 interface
 
-    # msg contains already parsed command in ALP in JSON
-    #print("ALP Command received from {}".format(msg.topic))
-    try:
-      obj = jsonpickle.json.loads(msg.payload)
-    except:
-      #print("Payload not valid JSON, skipping") # TODO issue with TB rule filter, to be fixed
-      return
+        # get remote node id (when this is received over D7 interface)
+        if cmd.interface_status != None and cmd.interface_status.operand.interface_id == 0xd7:
+            # Convert from decimal to hexadecimal
+            node_id = '{:x}'.format(cmd.interface_status.operand.interface_status.addressee.id)
 
-    gateway = obj["deviceId"]
-    cmd = jsonpickle.decode(jsonpickle.json.dumps(obj["alp"]))
-    node_id = gateway  # overwritten below with remote node ID when received over D7 interface
+        if node_id == self.config.node:
+            print("***Right node: {}".format(node_id))
 
-    # get remote node id (when this is received over D7 interface)
-    if cmd.interface_status != None and cmd.interface_status.operand.interface_id == 0xd7:
-      #Convert from decimal to hexadecimal
-      node_id = '{:x}'.format(cmd.interface_status.operand.interface_status.addressee.id)
+            for action in cmd.actions:
+                if type(action.operation) is ReturnFileData and action.operand.offset.id == 1:
 
-    if node_id == self.config.node:
-      print("***Right node: {}".format(node_id))
-      # look for returned file data which we can parse, in this example file 1
-      my_sensor_value = 0
+                    length = len(action.operand.data)
+                    fmt = "c" * length
+                    value_type = struct.unpack(fmt, bytearray(action.operand.data))[
+                        0]  # parse binary payload (adapt to your needs)
+                    print("Incoming data is from kind", format(value_type))
 
-      for action in cmd.actions:
-        if type(action.operation) is ReturnFileData and action.operand.offset.id == 1:
-          my_sensor_value = struct.unpack("cccc", bytearray(action.operand.data))[teller]  # parse binary payload (adapt to your needs)
-          print("node {} sensor value {}".format(node_id, my_sensor_value))
-          
-          if teller < 3:
-            teller = teller+1
-          else:
-            teller = 0
+                    if value_type == "M":  # Magnetometer information
+                        magn_value1 = struct.unpack(fmt, bytearray(action.operand.data))[1]
+                        magn_value2 = struct.unpack(fmt, bytearray(action.operand.data))[2]
+                        magn_value = int('{0:08b}'.format(ord(magn_value1)) + '{0:08b}'.format(ord(magn_value2)), 2)
+                        # {} places a variable into a string, 0 takes the variable at argument position 0,
+                        # : adds formatting options for this variable (otherwise it would represent decimal 6),
+                        # 08 formats the number to eight digits zero-padded on the left,
+                        # b converts the number to its binary representation
+                        dir = self.magnetometer_to_direction(magn_value)
+                        print("Direction is:", format(dir))
+                        json_str = {"direction": dir}
+                    elif value_type == "F": #RSSI value for fingerprinting
+                        json_str = {"XPos": x}
 
-      # save the parsed sensor data as an attribute to the device, using the TB API
-      try:
-        # first get the deviceId mapped to the device name
-        response = self.device_controller_api.get_tenant_device_using_get(device_name=str(node_id))
-        device_id = response.id.id
-        #print(device_id)
+            # save the parsed sensor data as an attribute to the device, using the TB API
+            try:
+                # first get the deviceId mapped to the device name
+                response = self.device_controller_api.get_tenant_device_using_get(device_name=str(node_id))
+                device_id = response.id.id
+                # print(device_id)
 
-        # next, get the access token of the device
-        response = self.device_controller_api.get_device_credentials_by_device_id_using_get(device_id=device_id)
-        device_access_token = response.credentials_id
-        #print(device_access_token)
+                # next, get the access token of the device
+                response = self.device_controller_api.get_device_credentials_by_device_id_using_get(device_id=device_id)
+                device_access_token = response.credentials_id
+                # print(device_access_token)
 
-        # finally, store the sensor attribute on the node in TB
-        response = self.device_api_controller_api.post_telemetry_using_post(
-          device_token=device_access_token,
-          json={"my_sensor": my_sensor_value}
-        )
+                # finally, store the sensor attribute on the node in TB
+                response = self.device_api_controller_api.post_telemetry_using_post(
+                    device_token=device_access_token,
+                    json=json_str
+                )
 
-        print("Updated my_sensor attribute for node {}".format(node_id))
-      except ApiException as e:
-        print("Exception when calling API: %s\n" % e)
-    else:
-      #print("Wrong node: {}".format(node_id))
-      return
+                print("Updated my_sensor attribute for node {}".format(node_id))
+            except ApiException as e:
+                print("Exception when calling API: %s\n" % e)
+        else:
+            # print("Wrong node: {}".format(node_id))
+            return
 
+    def magnetometer_to_direction(self, magn_value):
+        if 338 < magn_value or magn_value <= 23:
+            direction = "N"
+        elif 23 < magn_value or magn_value <= 68:
+            direction = "NE"
+        elif 68 < magn_value or magn_value <= 113:
+            direction = "E"
+        elif 113 < magn_value or magn_value <= 158:
+            direction = "SE"
+        elif 158 < magn_value or magn_value <= 203:
+            direction = "S"
+        elif 203 < magn_value or magn_value <= 248:
+            direction = "SW"
+        elif 248 < magn_value or magn_value <= 293:
+            direction = "W"
+        elif 293 < magn_value or magn_value <= 338:
+            direction = "NW"
+        else:
+            direction = "NULL"
 
-  def __del__(self):
-    try:
-      self.mq.loop_stop()
-      self.mq.disconnect()
-    except:
-      pass
+        return direction
 
-  def run(self):
-    print("Started")
-    keep_running = True
-    while keep_running:
-      try:
-        signal.pause()
-      except KeyboardInterrupt:
-        print("received KeyboardInterrupt... stopping processing")
-        keep_running = False
+    def __del__(self):
+        try:
+            self.mq.loop_stop()
+            self.mq.disconnect()
+        except:
+            pass
+
+    def run(self):
+        print("Started")
+        keep_running = True
+        while keep_running:
+            try:
+                signal.pause()
+            except KeyboardInterrupt:
+                print("received KeyboardInterrupt... stopping processing")
+                keep_running = False
 
 
 if __name__ == "__main__":
-  Backend().run()
+    Backend().run()
