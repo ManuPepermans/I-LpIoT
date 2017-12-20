40a2f23e597c2c6b6c9284cfee1d30de5d32cd3c
Kevin Van de Mieroop <kevinvdm@msn.com>
2017-12-20 16:26:08 +0100

    Buzzer project added

---
+++ BuzzerTest/Debug/Src/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/stm32l1xx_hal_msp.c \
+../Src/stm32l1xx_it.c \
+../Src/system_stm32l1xx.c 
+
+OBJS += \
+./Src/main.o \
+./Src/stm32l1xx_hal_msp.o \
+./Src/stm32l1xx_it.o \
+./Src/system_stm32l1xx.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/stm32l1xx_hal_msp.d \
+./Src/stm32l1xx_it.d \
+./Src/system_stm32l1xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed="__attribute__((__packed__))"' -DUSE_HAL_DRIVER -DSTM32L152xE -I"/Users/kevinvdm/Desktop/iwdgtest/BuzzerTest/Inc" -I"/Users/kevinvdm/Desktop/iwdgtest/BuzzerTest/Drivers/STM32L1xx_HAL_Driver/Inc" -I"/Users/kevinvdm/Desktop/iwdgtest/BuzzerTest/Drivers/STM32L1xx_HAL_Driver/Inc/Legacy" -I"/Users/kevinvdm/Desktop/iwdgtest/BuzzerTest/Drivers/CMSIS/Device/ST/STM32L1xx/Include" -I"/Users/kevinvdm/Desktop/iwdgtest/BuzzerTest/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
