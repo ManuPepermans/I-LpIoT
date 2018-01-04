b168a7dc6a671f39e489403f55d0352df688bf0f
Unknown <levi.marien@gmail.com>
2018-01-04 21:55:56 +0100

    /

--- Ecompass/html/search/variables_6.js
+++ Ecompass/html/search/variables_6.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['y',['y',['../main_8h.html#aa4f0d3eebc3c443f9be81bf48561a217',1,'main.h']]],
   ['yawdegree',['yawDegree',['../main_8h.html#a57d91bb32b0f9d721e59f75166c67835',1,'main.h']]],
   ['yawrad',['yawRad',['../main_8h.html#a5501dfebb1ddbd0a10ae85b4f139977f',1,'main.h']]],
   ['yawtemp',['yawTemp',['../main_8h.html#a8d274e0675936fa20b09bb338ee610b9',1,'main.h']]]
