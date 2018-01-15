5c9896b0ecf80582901a25b138b3a7a4225e85b9
Unknown <levi.marien@gmail.com>
2018-01-15 06:00:24 +0100

    Added testcode for in an houre in comment

--- Final/src/main.c
+++ Final/src/main.c
@@ -962,6 +962,43 @@ static void MX_GPIO_Init(void) {
 	GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
 	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+//	/*Configure GPIO pins : PC14 PC15 PC0 PC1
+//		 PC2 PC3 PC4 PC5
+//		 PC6 PC8 PC9 */
+//		GPIO_InitStruct.Pin = GPIO_PIN_14 | GPIO_PIN_15 | GPIO_PIN_0 | GPIO_PIN_1
+//				| GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6
+//				| GPIO_PIN_8 | GPIO_PIN_9;
+//		GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+//		GPIO_InitStruct.Pull = GPIO_NOPULL;
+//		HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+//
+//		/*Configure GPIO pins : PH0 PH1 */
+//		GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+//		GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+//		GPIO_InitStruct.Pull = GPIO_NOPULL;
+//		HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+//
+//		/*Configure GPIO pins : PA0 PA1 PA4
+//		 PA6 PA7 PA8 PA9
+//		 PA10 PA11 PA12 PA13
+//		 PA14 PA15 */
+//		GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_6
+//				| GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11
+//				| GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+//		GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+//		GPIO_InitStruct.Pull = GPIO_NOPULL;
+//		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+//
+//		/*Configure GPIO pins : PB0 PB1 PB2 PB12
+//		 PB13 PB14 PB15 PB4
+//		 PB5 PB7 */
+//		GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_12
+//				| GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15 | GPIO_PIN_4 | GPIO_PIN_5
+//				| GPIO_PIN_7;
+//		GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+//		GPIO_InitStruct.Pull = GPIO_NOPULL;
+//		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 }
 
 /**
